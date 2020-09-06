1e623418204620e3534686fc4f2f3090cb01d9d2
techno-tanoC <develop.tanoc@gmail.com>
2020-09-06 16:39:44 +0900

    set viewport

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,14 @@ export default {
 
   components: true,
 
+  head: {
+    title: 'TanoC Blog',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes' },
+    ]
+  },
+
   env: {
     NODE_ENV: process.env.NODE_ENV
   },
